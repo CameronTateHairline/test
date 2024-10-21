03f1f068d11835ac48a11484284cc44267807e6a
Kale Baker <kbaker624@canon-slade.bolton.sch.uk>
2024-10-21 09:56:43 +0100

    shapeslesson

---
+++ mysite/script.js
@@ -0,0 +1,48 @@
+function slide1() {
+    document.getElementById("image1").src =
+    "Images/squaretest1.png";
+    document.getElementById("text1").innerHTML = 
+    "Red Square";
+}
+
+function slide2() {
+    document.getElementById("image1").src =
+    "Images/bluetriangle1.png";
+    document.getElementById("text1").innerHTML = 
+    "Blue Triangle";
+}
+
+function slide3() {
+    document.getElementById("image1").src =
+    "Images/yellowcircle1.png";
+    document.getElementById("text1").innerHTML = 
+    "Yellow Circle";
+}
+
+function slide4() {
+    document.getElementById("image1").src =
+    "Images/greenrectangle.png";
+    document.getElementById("text1").innerHTML = 
+    "Green Rectangle";
+}
+
+function slide5() {
+    document.getElementById("image1").src =
+    "Images/orangehexagon1.png";
+    document.getElementById("text1").innerHTML = 
+    "Orange Hexagon";
+}
+
+function slide6() {
+    document.getElementById("image1").src =
+    "Images/purplestar1.png";
+    document.getElementById("text1").innerHTML = 
+    "Purple Star";
+}
+
+function slide7() {
+    document.getElementById("image1").src =
+    "Images/thatsallfolks.jpg";
+    document.getElementById("text1").innerHTML = 
+    "The End";
+}
